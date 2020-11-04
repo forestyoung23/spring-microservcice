43ced12f0cecb3b9e1ed56e9958c9c1dbbf0f9e1
Forest <forestyoung23@gmail.com>
2020-11-05 02:17:32 +0800

    redis分布式锁

---
+++ spring-cloud-discovery/eureka-service/src/main/java/com/forest/eurekaservice/service/redislock/RedisLocckDemo.java
@@ -0,0 +1,24 @@
+package com.forest.eurekaservice.service.redislock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Forest
+ * @date 2020年11月05日 02:14
+ */
+@Controller
+public class RedisLocckDemo {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @RequestMapping("/reduce")
+    public String reduceStock() {
+//        redisTemplate.opsForValue().setIfAbsent("")
+        return "1";
+    }
+}
