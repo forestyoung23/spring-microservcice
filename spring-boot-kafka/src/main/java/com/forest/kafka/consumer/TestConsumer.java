11aa81dd8566fc8ec4aee112694b927342226073
Forest <forestyoung23@gmail.com>
2021-07-13 17:45:15 +0800

    Kafka整合

---
+++ spring-boot-kafka/src/main/java/com/forest/kafka/consumer/TestConsumer.java
@@ -0,0 +1,51 @@
+package com.forest.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author dongyang
+ * @date 2021年07月13日 16:28
+ */
+public class TestConsumer {
+    private static Properties getProperties() {
+        Properties props = new Properties();
+        // 服务端ip及端口
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        // 消费者分组id
+        props.setProperty("group.id", "test");
+        // Consumer 在commit offset时有两种模式：自动提交，手动提交。
+        // 手动提交。
+        // 自动提交：是Kafka Consumer会在后台周期性的去commit。默认值是true。
+        props.setProperty("enable.auto.commit", "true");
+        // 自动提交间隔。范围：[0,Integer.MAX]，默认值是 5000 （5 s）
+        // 消费者偏移的频率以毫秒为单位自动提交给Kafka，如果enable.auto.commit设置为true。
+        props.setProperty("auto.commit.interval.ms", "1000");
+        // 表示使用StringSerializer对key进行反序列化
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // 表示使用StringSerializer对value进行反序列化
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
+
+    public static void testConsumer() {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(getProperties());
+        consumer.subscribe(Arrays.asList("Test"));
+        while (true) {
+            // 设置消费者在返回前等待的时间不超过100ms
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record.toString());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        testConsumer();
+    }
+}
---
+++ spring-boot-kafka/src/main/java/com/forest/kafka/producer/TestProducer.java
@@ -0,0 +1,49 @@
+package com.forest.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @author dongyang
+ * @date 2021年07月13日 15:52
+ */
+public class TestProducer {
+    private static Properties getProperties() {
+        // 创建properties对象
+        Properties props = new Properties();
+        // 服务端ip及端口
+        props.put("bootstrap.servers", "localhost:9092");
+        /*
+        指定了必须要有多少个分区副本收到消息，生产者才认为该消息是写入成功的，这个参数对于消息是否丢失起着重要作用，该参数的配置具体如下：
+        - 0 : 表示生产者在成功写入消息之前不会等待任何来自服务器的响应. 换句话说，一旦出现了问题导致服务器没有收到消息，那么生产者就无从得知，消息也就丢失了.
+              该配置由于不需要等到服务器的响应，所以可以以网络支持的最大速度发送消息，从而达到很高的吞吐量。
+        - 1 : 表示只要集群的leader分区副本接收到了消息，就会向生产者发送一个成功响应的ack，此时生产者接收到ack之后就可以认为该消息是写入成功的.
+              一旦消息无法写入leader分区副本(比如网络原因、leader节点崩溃),生产者会收到一个错误响应，当生产者接收到该错误响应之后，为了避免数据丢失，会重新发送数据.这种方式的吞吐量取决于使用的是异步发送还是同步发送.
+        - all : 表示只有所有参与复制的节点(ISR列表的副本)全部收到消息时，生产者才会接收到来自服务器的响应.
+                这种模式是最高级别的，也是最安全的，可以确保不止一个Broker接收到了消息. 该模式的延迟会很高.
+        */
+        props.put("acks", "all");
+        // 表示使用StringSerializer对key进行序列化
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // 表示使用StringSerializer对value进行序列化
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        return props;
+    }
+
+    public static void testPublish() {
+        // 根据配置创建生产者
+        KafkaProducer<Object, Object> producer = new KafkaProducer<>(getProperties());
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord record = new ProducerRecord<>("Test", Integer.toString(i), Integer.toString(i));
+            // 发送消息到指定Topic
+            producer.send(record);
+        }
+        producer.close();
+    }
+
+    public static void main(String[] args) {
+        testPublish();
+    }
+}
