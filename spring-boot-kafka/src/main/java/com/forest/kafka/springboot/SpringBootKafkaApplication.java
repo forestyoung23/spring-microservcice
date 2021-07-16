419e3738e126216a4d07f4ec50a9d0ab6d170233
Forest <forestyoung23@gmail.com>
2021-07-16 17:33:30 +0800

    1.升级SpringBoot版本 2.引入spring-kafka

--- spring-boot-kafka/src/main/java/com/forest/kafka/springboot/SpringBootKafkaApplication.java
+++ spring-boot-kafka/src/main/java/com/forest/kafka/springboot/SpringBootKafkaApplication.java
@@ -1,21 +1,60 @@
 package com.forest.kafka.springboot;
 
-import com.forest.kafka.springboot.producer.SpringBootProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
 
 /**
  * @author Forest
  * @date 2021年07月16日 11:56
  */
+@Slf4j
 @SpringBootApplication
 public class SpringBootKafkaApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootKafkaApplication.class, args);
-        SpringBootProducer springBootProducer = (SpringBootProducer) applicationContext.getBean("springBootProducer");
-        springBootProducer.publish("test", "forest", "kiss my ass");
+//        SpringBootProducer springBootProducer = (SpringBootProducer) applicationContext.getBean("springBootProducer");
+//        springBootProducer.publish("test", "forest", "kiss my ass");
     }
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return  args -> {
+            for (int i = 0; i < 10; i++) {
+                template.send("topic1", "test");
+                log.info("send message successed");
+            }
+        };
+    }
+
+//    @Bean
+//    public NewTopic topic() {
+//        return TopicBuilder.name("topic1")
+//                .partitions(10)
+//                .replicas(1)
+//                .build();
+//    }
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.err.println(in);
+    }
+
 
 }
--- spring-boot-kafka/src/main/java/com/forest/kafka/springboot/producer/SpringBootProducer.java
+++ spring-boot-kafka/src/main/java/com/forest/kafka/springboot/producer/SpringBootProducer.java
@@ -1,26 +1,26 @@
-package com.forest.kafka.springboot.producer;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-
-
-/**
- * @author Forest
- * @date 2021年07月16日 16:15
- */
-@Slf4j
-@Component
-public class SpringBootProducer {
-    @Resource
-    private KafkaProducer producer;
-
-    public void publish(String topic, String key, String value) {
-        ProducerRecord record = new ProducerRecord<String, String>(topic, key, value);
-        producer.send(record);
-        log.info("send message successed, topic:{},key:{},value:{}", topic, key, value);
-    }
-}
+//package com.forest.kafka.springboot.producer;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.kafka.clients.producer.KafkaProducer;
+//import org.apache.kafka.clients.producer.ProducerRecord;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.Resource;
+//
+//
+///**
+// * @author Forest
+// * @date 2021年07月16日 16:15
+// */
+//@Slf4j
+//@Component
+//public class SpringBootProducer {
+//    @Resource
+//    private KafkaProducer producer;
+//
+//    public void publish(String topic, String key, String value) {
+//        ProducerRecord record = new ProducerRecord<String, String>(topic, key, value);
+//        producer.send(record);
+//        log.info("send message successed, topic:{},key:{},value:{}", topic, key, value);
+//    }
+//}
