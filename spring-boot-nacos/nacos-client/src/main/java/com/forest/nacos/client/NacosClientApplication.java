e52b413f86d1fc412382561d2483b83aeec1dfbc
Forest <forestyoung23@gmail.com>
2022-06-17 09:09:43 +0800

    feat: nacos整合

---
+++ spring-boot-nacos/nacos-client/src/main/java/com/forest/nacos/client/NacosClientApplication.java
@@ -0,0 +1,15 @@
+package com.forest.nacos.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosClientApplication.class, args);
+    }
+
+}
---
+++ spring-boot-nacos/nacos-server/src/main/java/com/forest/nacos/server/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.forest.nacos.server.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Forest Dong
+ * @date 2022年06月16日 10:41
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+    // @NacosValue()
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
